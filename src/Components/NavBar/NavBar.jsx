75e2e15d55da472b7732f973acdba1dfbbfbbc51
Jhon Fredy <jhonfcompu@gmail.com>
2025-12-03 09:47:45 -0500

    Se agrega Nabvar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,49 @@
+import "./NavBar.css"
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+
+  return (
+
+    <section className='header'>
+
+        <h1 className='logo'>|JF<span>.SHOP|</span></h1>
+
+        <nav className="navbar">
+            <ul className="nav-links">
+               <li>
+                 <Link to = "/">Home</Link>
+               </li>
+
+               <li>
+                  <Link to = "/Colletions">Collections</Link>
+                </li>
+
+                <li>
+                  <Link to = "/About">About</Link>
+                </li>
+
+                 <li>
+                  <Link to = "/">Contact</Link>
+                </li> 
+            </ul>
+        </nav>
+
+        <div className="icons">
+
+          <button className="search-button">
+            <i className="fas fa-search"></i>
+          </button>
+
+         <Link to = "/carrito" className="icon-button">
+         <i className="fas fa-shopping-cart"></i>
+         <span className="counter">0</span>         
+         </Link>
+
+        </div>
+
+    </section>
+  )
+}
+
+
